8a8509ad4976ee5e21e80d39936cc4f0bd5620ea
AhmadSon <ahmad05102001@gmail.com>
2023-05-21 20:16:00 +0700

    add all files

---
+++ lab11_php_ci/ci4/app/Views/template/contact.php
@@ -0,0 +1,12 @@
+<?= $this->include('tamplate/header'); ?>
+
+<h1><?= $title; ?></h1>
+<hr>
+<p><?= $content; ?></p>
+<br>
+
+<h1>
+  example@gmail.com
+</h1>
+
+<?= $this->include('tamplate/footer'); ?>
